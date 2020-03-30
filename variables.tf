76e9a84007c37b404f2572e0456dafe74144d37b
SrinathDM <srinathd@decisionminds.com>
2020-03-30 11:14:54 -0600

    updated
    updated

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "gcp_project_id" {
+  description = "The ID of the GCP project in which these resources will be created."
+}
+
+variable "instance_name" {
+  description = "The Name to use for the Cloud Instance."
+  type        = string
+  default     = "terratest-example"
+}
+
+variable "machine_type" {
+  description = "The Machine Type to use for the Cloud Instance."
+  type        = string
+  default     = "f1-micro"
+}
+
+variable "zone" {
+  description = "The Zone to launch the Cloud Instance into."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "bucket_name" {
+  description = "The Name of the example Bucket to create."
+  type        = string
+  default     = "example-terratest-bucket"
+}
+
+variable "bucket_location" {
+  description = "The location to store the Bucket. This value can be regional or multi-regional."
+  type        = string
+  default     = "US"
+}
